54a19c2ff75591c7a0e00447e75f2a457ac170b3
Your Name <s.r.j.s.n2017@gmail.com>
2024-10-30 20:40:19 +0530

    submit 1

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>This is a sample React Router program.</h1>
+    </div>
+  );
+};
+
+export default About;
